a0ca0fa380b5fa0cdf4ae057f0a468ed1642a789
FlyingPig13 <anotherhale.m@gmail.com>
2016-02-02 11:36:39 -0500

    Randomized color
    Randomized color for painting is working as intended

--- sketch.js
+++ sketch.js
@@ -1,7 +1,22 @@
 function setup () {
   createCanvas(600,600);
+  background(40, 40, 200);
+ fill((random(255)), (random(255)), (random(255)));
 }
 
 function draw() {
+ 
+strokeWeight(0);
+  function painter(){
+    if (mouseIsPressed){
+     ellipse(mouseX, mouseY, random(50), random(50));
+      }
+      else{ fill((random(255)), (random(255)), (random(255)));
+      }
+    }
+
+painter()
+  
+ 
 
 }
