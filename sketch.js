78f9f9ecfd505153ad7cf137d0da3efffc7fdbc5
FlyingPig13 <anotherhale.m@gmail.com>
2016-02-02 12:43:47 -0500

    Brush Weight
    Trying to get variable stroke weights on painting to work

--- sketch.js
+++ sketch.js
@@ -1,17 +1,37 @@
+ 
+
 function setup () {
   createCanvas(600,600);
-  background(40, 40, 200);
+  background(255, 255, 255);
  fill((random(255)), (random(255)), (random(255)));
+
 }
 
 function draw() {
- 
+  
+
+
 strokeWeight(0);
-  function painter(){
+
+var brushWeight = function(n){
+  if(keyCode == UP_ARROW){
+    n + 1
+  }
+  else{ if(keyCode == DOWN_ARROW){
+    n - 1
+  }
+  else{ n
+  }
+}
+}
+
+var painter = function(){
     if (mouseIsPressed){
-     ellipse(mouseX, mouseY, random(50), random(50));
+      ellipse(mouseX, mouseY, brushWeight(1), brushWeight(1));
       }
       else{ fill((random(255)), (random(255)), (random(255)));
+      
+      
       }
     }
 
